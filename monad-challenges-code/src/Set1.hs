f4701abee7cb5b7b8efd3924e8946536ba3aef10
Leo Zhang <leo@leozhang.me>
2021-04-02 00:31:13 -0700

    feat: 1-4

--- monad-challenges-code/src/Set1.hs
+++ monad-challenges-code/src/Set1.hs
@@ -4,6 +4,8 @@ module Set1
     randEven,
     randOdd,
     randTen,
+    randPair,
+    randPair_,
   )
 where
 
@@ -37,3 +39,26 @@ randTen = generalA (* 10) rand
 
 generalA :: (a -> b) -> Gen a -> Seed -> (b, Seed)
 generalA f r s = let (x, s') = r s in (f x, s')
+
+randPair :: Gen (Char, Integer)
+randPair s = ((c, n), s'')
+  where
+    (c, s') = randLetter s
+    (n, s'') = rand s'
+
+generalPair :: Gen a -> Gen b -> Gen (a, b)
+generalPair f g s = ((a, b), s'')
+  where
+    (a, s') = f s
+    (b, s'') = g s'
+
+randPair_ :: Gen (Char, Integer)
+randPair_ = generalPair randLetter rand
+
+generalB :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB f g1 g2 s = (f a b, s'')
+  where
+    ((a, b), s'') = generalPair g1 g2 s
+
+generalPair2 :: Gen a -> Gen b -> Gen (a, b)
+generalPair2 = generalB (,)
--- monad-challenges-code/test/Main.hs
+++ monad-challenges-code/test/Main.hs
@@ -7,7 +7,7 @@ import Data.ByteString.Base16 (encode)
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 import MCPrelude (mkSeed)
-import Set1 (fiveRands, randEven, randOdd, randString3, randTen)
+import Set1 (fiveRands, randEven, randOdd, randPair, randString3, randTen, randPair_)
 import Test.Hspec (describe, hspec, it, shouldBe)
 
 main :: IO ()
@@ -16,3 +16,5 @@ main = hspec $ do
     it "fiveRands" $ product fiveRands `shouldBe` 8681089573064486461641871805074254223660
     it "randString3" $ encode (hash $ encodeUtf8 $ T.pack randString3) `shouldBe` "9d475eb78d3e38085220ed6ebde9d8f7d26540bb1c8f9382479c3acd4c8c94a3"
     it "rand{Even,Odd,Ten}" $ product ((\f -> fst $ f $ mkSeed 1) <$> [randEven, randOdd, randTen]) `shouldBe` 189908109902700
+    it "randPair" $ fst (randPair $ mkSeed 1) `shouldBe` ('l', 282475249)
+    it "randPair_" $ fst (randPair_ $ mkSeed 1) `shouldBe` ('l', 282475249)
