aa0af26f7f7120bfe048f369771a67987d798d7f
Leo Zhang <leo@leozhang.me>
2021-04-02 00:22:54 -0700

    feat: rand{Even,Odd,Ten}

--- monad-challenges-code/src/Set1.hs
+++ monad-challenges-code/src/Set1.hs
@@ -1,8 +1,17 @@
-module Set1 (fiveRands, randString3) where
+module Set1
+  ( fiveRands,
+    randString3,
+    randEven,
+    randOdd,
+    randTen,
+  )
+where
 
 import MCPrelude (Seed, mkSeed, rand, toLetter)
 
-randsFromGen :: (Seed -> (t, Seed)) -> Seed -> [(t, Seed)]
+type Gen t = Seed -> (t, Seed)
+
+randsFromGen :: Gen t -> Seed -> [(t, Seed)]
 randsFromGen f seed = tail $ iterate (\(x, s) -> f s) (undefined, seed)
 
 rands :: Seed -> [(Integer, Seed)]
@@ -11,8 +20,20 @@ rands = randsFromGen rand
 fiveRands :: [Integer]
 fiveRands = take 5 $ fmap fst $ rands $ mkSeed 1
 
-randLetter :: Seed -> (Char, Seed)
+randLetter :: Gen Char
 randLetter seed = let (n, s) = rand seed in (toLetter n, s)
 
 randString3 :: String
 randString3 = take 3 $ fmap fst $ randsFromGen randLetter $ mkSeed 1
+
+randEven :: Gen Integer
+randEven = generalA (* 2) rand
+
+randOdd :: Gen Integer
+randOdd = generalA (+ 1) randEven
+
+randTen :: Gen Integer
+randTen = generalA (* 10) rand
+
+generalA :: (a -> b) -> Gen a -> Seed -> (b, Seed)
+generalA f r s = let (x, s') = r s in (f x, s')
--- monad-challenges-code/test/Main.hs
+++ monad-challenges-code/test/Main.hs
@@ -6,7 +6,8 @@ import Crypto.Hash.SHA256 (hash)
 import Data.ByteString.Base16 (encode)
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
-import Set1 (fiveRands, randString3)
+import MCPrelude (mkSeed)
+import Set1 (fiveRands, randEven, randOdd, randString3, randTen)
 import Test.Hspec (describe, hspec, it, shouldBe)
 
 main :: IO ()
@@ -14,3 +15,4 @@ main = hspec $ do
   describe "Set 1: Random Numbers" $ do
     it "fiveRands" $ product fiveRands `shouldBe` 8681089573064486461641871805074254223660
     it "randString3" $ encode (hash $ encodeUtf8 $ T.pack randString3) `shouldBe` "9d475eb78d3e38085220ed6ebde9d8f7d26540bb1c8f9382479c3acd4c8c94a3"
+    it "rand{Even,Odd,Ten}" $ product ((\f -> fst $ f $ mkSeed 1) <$> [randEven, randOdd, randTen]) `shouldBe` 189908109902700
