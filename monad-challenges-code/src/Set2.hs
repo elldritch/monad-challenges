7cba1fdfc4045dee2611b6bd9c3a3415ec99106d
Leo Zhang <leo@leozhang.me>
2023-08-13 01:53:24 -0700

    Set 2

---
+++ monad-challenges-code/src/Set2.hs
@@ -0,0 +1,159 @@
+module Set2 (
+  Maybe (..),
+  queryGreek,
+  queryGreek2,
+  addSalaries,
+  addSalaries2,
+  tailProd,
+  tailSum,
+  tailMax,
+  tailMin,
+) where
+
+import MCPrelude (
+  Bool (False, True),
+  Double,
+  Eq ((==)),
+  Fractional ((/)),
+  GreekData,
+  Integer,
+  Num ((+)),
+  Ord (max, min),
+  Show (show),
+  String,
+  flip,
+  foldr,
+  fromInteger,
+  fromIntegral,
+  ifThenElse,
+  product,
+  sum,
+  undefined,
+  ($),
+  (++),
+  (.),
+ )
+
+-- Part 1
+
+data Maybe t = Just t | Nothing
+
+instance Show a => Show (Maybe a) where
+  show (Just t) = "Just " ++ show t
+  show Nothing = "Nothing"
+
+instance Eq a => Eq (Maybe a) where
+  Just a == Just b = a == b
+  Nothing == Nothing = True
+  _ == _ = False
+
+-- Part 2
+
+headMay :: [a] -> Maybe a
+headMay (x : _) = Just x
+headMay _ = Nothing
+
+tailMay :: [a] -> Maybe [a]
+tailMay (_ : x) = Just x
+tailMay _ = Nothing
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay x ((a, b) : xs) = if a == x then Just b else lookupMay x xs
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0 = Nothing
+divMay a b = Just $ a / b
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay = foldr f Nothing
+ where
+  f :: Ord a => a -> Maybe a -> Maybe a
+  f x Nothing = Just x
+  f x (Just y) = Just $ max x y
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay = foldr f Nothing
+ where
+  f :: Ord a => a -> Maybe a -> Maybe a
+  f x Nothing = Just x
+  f x (Just y) = Just $ min x y
+
+-- Part 3
+
+queryGreek :: GreekData -> String -> Maybe Double
+queryGreek d key = case xsHead of
+  Nothing -> Nothing
+  Just h -> case xsTailMax of
+    Nothing -> Nothing
+    Just t -> divMay (fromIntegral t) (fromIntegral h)
+ where
+  xs = lookupMay key d
+  xsTail = case xs of
+    Just ns -> tailMay ns
+    Nothing -> Nothing
+  xsTailMax = case xsTail of
+    Just ns -> maximumMay ns
+    Nothing -> Nothing
+  xsHead = case xs of
+    Just ns -> headMay ns
+    Nothing -> Nothing
+
+-- Part 4
+
+chain :: (a -> Maybe b) -> Maybe a -> Maybe b
+chain f ma = case ma of
+  Nothing -> Nothing
+  Just a -> f a
+
+link :: Maybe a -> (a -> Maybe b) -> Maybe b
+link = flip chain
+
+queryGreek2 :: GreekData -> String -> Maybe Double
+queryGreek2 d key = xsTailMax `link` \t -> xsHead `link` \h -> divMay (fromIntegral t) (fromIntegral h)
+ where
+  xs = lookupMay key d
+  xsTailMax = xs `link` tailMay `link` maximumMay
+  xsHead = xs `link` headMay
+
+-- Part 5
+
+addSalaries :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries d a b = mx `link` \x -> my `link` \y -> mkMaybe $ x + y
+ where
+  mx = lookupMay a d
+  my = lookupMay b d
+
+yLink :: Maybe a -> (a -> b) -> Maybe b
+yLink ma f = ma `link` \a -> mkMaybe $ f a
+
+addSalaries2 :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries2 d a b = mx `link` \x -> my `yLink` \y -> x + y
+ where
+  mx = lookupMay a d
+  my = lookupMay b d
+
+mkMaybe :: a -> Maybe a
+mkMaybe = Just
+
+-- Part 6
+
+tailProd :: Num a => [a] -> Maybe a
+tailProd xs = tailMay xs `yLink` product
+
+tailSum :: Num a => [a] -> Maybe a
+tailSum xs = tailMay xs `yLink` sum
+
+transMaybe :: (a -> b) -> Maybe a -> Maybe b
+transMaybe f ma = ma `yLink` f
+
+tailMax :: Ord a => [a] -> Maybe (Maybe a)
+tailMax = transMaybe maximumMay . tailMay
+
+tailMin :: Ord a => [a] -> Maybe (Maybe a)
+tailMin = transMaybe minimumMay . tailMay
+
+combine :: Maybe (Maybe a) -> Maybe a
+combine mm = case mm of
+  Nothing -> Nothing
+  Just m -> m
--- monad-challenges-code/test/Main.hs
+++ monad-challenges-code/test/Main.hs
@@ -2,15 +2,19 @@
 
 module Main (main) where
 
+import Prelude hiding (Maybe (..))
+
 import Crypto.Hash.SHA256 (hash)
 import Data.ByteString.Base16 (encode)
+import Data.Foldable (forM_)
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 import Test.Hspec (describe, hspec, it, shouldBe)
 
-import MCPrelude (mkSeed)
+import MCPrelude (greekDataA, greekDataB, mkSeed, salaries)
 
 import Set1 (fiveRands, randEven, randOdd, randPair, randPair_, randString3, randString3', randTen)
+import Set2 (Maybe (..), addSalaries, addSalaries2, queryGreek, queryGreek2, tailMax, tailMin, tailProd, tailSum)
 
 main :: IO ()
 main = hspec $ do
@@ -21,3 +25,51 @@ main = hspec $ do
     it "randPair" $ fst (randPair $ mkSeed 1) `shouldBe` ('l', 282475249)
     it "randPair_" $ fst (randPair_ $ mkSeed 1) `shouldBe` ('l', 282475249)
     it "randString3'" $ encode (hash $ encodeUtf8 $ T.pack $ fst randString3') `shouldBe` "9d475eb78d3e38085220ed6ebde9d8f7d26540bb1c8f9382479c3acd4c8c94a3"
+
+  describe "Set 2: Failing Computations" $ do
+    describe "queryGreek" $ testQueryGreek queryGreek
+    describe "queryGreek2" $ testQueryGreek queryGreek2
+    describe "addSalaries" $ testAddSalaries addSalaries
+    describe "addSalaries2" $ testAddSalaries addSalaries2
+    it "tailProd" $ do
+      tailProd [] `shouldBe` Nothing
+      tailProd [42] `shouldBe` Just 1
+      tailProd [2, 3, 5] `shouldBe` Just 15
+    it "tailSum" $ do
+      tailSum [] `shouldBe` Nothing
+      tailSum [42] `shouldBe` Just 0
+      tailSum [2, 3, 5] `shouldBe` Just 8
+    it "tailMax" $ do
+      tailMax [] `shouldBe` (Nothing :: Maybe (Maybe Integer))
+      tailMax [42] `shouldBe` Just Nothing
+      tailMax [2, 3, 5] `shouldBe` Just (Just 5)
+    it "tailMin" $ do
+      tailMin [] `shouldBe` (Nothing :: Maybe (Maybe Integer))
+      tailMin [42] `shouldBe` Just Nothing
+      tailMin [2, 3, 5] `shouldBe` Just (Just 3)
+ where
+  testQueryGreek f =
+    forM_ cases $
+      \(key, (dict, expected)) -> it key $ f dict key `shouldBe` expected
+   where
+    cases =
+      [ ("alpha", (greekDataA, Just 2.0))
+      , ("beta", (greekDataA, Nothing))
+      , ("gamma", (greekDataA, Just 3.3333333333333335))
+      , ("delta", (greekDataA, Nothing))
+      , ("zeta", (greekDataA, Nothing))
+      , ("rho", (greekDataB, Nothing))
+      , ("phi", (greekDataB, Just 0.24528301886792453))
+      , ("chi", (greekDataB, Just 9.095238095238095))
+      , ("psi", (greekDataB, Nothing))
+      , ("omega", (greekDataB, Just 24.0))
+      ]
+
+  testAddSalaries f = forM_ cases $ \(a, b, expected) -> it (show (a, b)) $ f salaries a b `shouldBe` expected
+   where
+    cases =
+      [ ("alice", "alice", Just 210000)
+      , ("alice", "bob", Just 195000)
+      , ("alice", "someone", Nothing)
+      , ("someone", "else", Nothing)
+      ]
